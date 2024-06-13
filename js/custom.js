c91677db70d05ea6bdd5ccd483bbacf30731a4cc
MartinsAmman <sayej_issa@yahoo.com>
2024-06-13 06:28:05 +0300

    Add files via upload

---
+++ js/custom.js
@@ -0,0 +1,49 @@
+
+  (function ($) {
+  
+  "use strict";
+
+    // NAVBAR
+    $('.navbar-collapse a').on('click',function(){
+      $(".navbar-collapse").collapse('hide');
+    });
+
+    $(function() {
+      $('.hero-slides').vegas({
+          slides: [
+            { src: 'images/slides/1.jpg' },
+            { src: 'images/slides/2.png' },
+            { src: 'images/slides/3.jpg' },
+            { src: 'images/slides/4.png' },
+            { src: 'images/slides/5.jpg' },
+            { src: 'images/slides/6.jpg' },
+            { src: 'images/slides/7.jpg' },
+          ],
+          timer: false,
+          animation: 'kenburns',
+      });
+    });
+    
+    // CUSTOM LINK
+    $('.smoothscroll').click(function(){
+      var el = $(this).attr('href');
+      var elWrapped = $(el);
+      var header_height = $('.navbar').height() + 60;
+  
+      scrollToDiv(elWrapped,header_height);
+      return false;
+  
+      function scrollToDiv(element,navheight){
+        var offset = element.offset();
+        var offsetTop = offset.top;
+        var totalScroll = offsetTop-navheight;
+  
+        $('body,html').animate({
+        scrollTop: totalScroll
+        }, 300);
+      }
+    });
+  
+  })(window.jQuery);
+
+
